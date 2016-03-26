761cfbd466670d826ac2e38b30794acce9e11a38
William Edward Gillespie <wgilles1@emich.edu>
2016-03-26 14:30:23 -0400

    File that stores constants for user commands such as "login", "logout", etc.

--- Chat/Constants.h
+++ Chat/Constants.h
@@ -8,6 +8,16 @@
 
 #ifndef Constants_h
 #define Constants_h
+/* constant commands a client can issue */
+#define HELP "help"
+#define LOGIN "login"
+#define LOGOUT "logout"
+#define WHO "who"
+#define STATUS "status"
+#define INVITE "invite"
+#define QUIT "quit"
+
+/* Sizes of types of messages the client can send */
 
 
 #endif /* Constants_h */
