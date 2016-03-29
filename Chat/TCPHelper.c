10d56a04d5218910ada29b51c5d47ea49f4d3890
William Gillespie <wgilles1@emich.edu>
2016-03-29 18:50:03 -0400

    TCPHandler is used as an abstraction to send and receive messages through tcp.

--- Chat/TCPHelper.c
+++ Chat/TCPHelper.c
@@ -5,5 +5,24 @@
 //  Created by  on 3/29/16.
 //  Copyright © 2016 EMU. All rights reserved.
 //
+// This file has methods for sending and receiving tcp messages
+
+#include <arpa/inet.h>
 
 #include "TCPHelper.h"
+#include "ClientToClientMessage.h"
+void DieWithError(char *errorMessage);  /* External error handling function */
+
+
+void sendTCPMessage(int socket, ClientToClientMessage clientToClientMessage){
+    long messageLength = sizeof(clientToClientMessage);
+    long bytesSent = send(socket, &clientToClientMessage, sizeof(clientToClientMessage), 0);
+    if (bytesSent != messageLength) {
+        DieWithError("message sent unexpected number of bytes; quit the program.\n");
+    }
+}
+
+ClientToClientMessage receiveTCPMessage(int socket){
+    ClientToClientMessage c2cMess;
+    return c2cMess;
+}
