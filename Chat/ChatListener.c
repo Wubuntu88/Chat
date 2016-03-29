e148edff60b0d1825b13f4bd59fdf37ce4cee2e1
William Gillespie <wgilles1@emich.edu>
2016-03-29 11:54:03 -0400

    Added the files for when the child process starts chatting.

---
+++ Chat/ChatListener.c
@@ -0,0 +1,9 @@
+//
+//  ChatListener.c
+//  Chat
+//
+//  Created by  on 3/29/16.
+//  Copyright © 2016 EMU. All rights reserved.
+//
+
+#include "ChatListener.h"
---
+++ Chat/ChatListener.h
@@ -0,0 +1,14 @@
+//
+//  ChatListener.h
+//  Chat
+//
+//  Created by  on 3/29/16.
+//  Copyright © 2016 EMU. All rights reserved.
+//
+
+#ifndef ChatListener_h
+#define ChatListener_h
+
+#include <stdio.h>
+
+#endif /* ChatListener_h */
