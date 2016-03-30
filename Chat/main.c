ee01b6a9608004ebe7a56a69b69d28e062a482c5
William Gillespie <wgilles1@emich.edu>
2016-03-30 19:52:06 -0400

    Testing stuff out with ctrl-d and sleeping threads for use in my other programs.

--- Chat/main.c
+++ Chat/main.c
@@ -7,9 +7,29 @@
 //
 
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    char str[20];
+    printf("enter something: ");
+    char* ptr = fgets(str, sizeof(str), stdin);
+    if (ptr == 0) {
+        printf("null pointer\n");
+    }
+    
+    printf("len: %lu", strlen(str));
+    /*
+    int makeMillis = 1000;
+    int numberMillis = 300;
+    int n = makeMillis * numberMillis;
+    for (int i = 1; i<=10; i++)
+    {
+        usleep(n);
+        printf("Hello, World!\n");
+        fflush(stdout);
+    }
+     */
     return 0;
 }
