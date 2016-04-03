e93d33984da2ea615fb2ea950dd1a6b8e5d4e9fd
William Edward Gillespie <wgilles1@emich.edu>
2016-04-03 01:33:48 -0400

    New module for the fork that listens for spontaneous messages from the server.

---
+++ Chat/serverMessageListener.c
@@ -0,0 +1,9 @@
+//
+//  serverMessageListener.c
+//  Chat
+//
+//  Created by William Edward Gillespie on 4/2/16.
+//  Copyright © 2016 EMU. All rights reserved.
+//
+
+#include "serverMessageListener.h"
---
+++ Chat/serverMessageListener.h
@@ -0,0 +1,14 @@
+//
+//  serverMessageListener.h
+//  Chat
+//
+//  Created by William Edward Gillespie on 4/2/16.
+//  Copyright © 2016 EMU. All rights reserved.
+//
+
+#ifndef serverMessageListener_h
+#define serverMessageListener_h
+
+#include <stdio.h>
+
+#endif /* serverMessageListener_h */
